openfrontio/OpenFrontIO

Reuse cached TradeShip paths for motion plans (#4654)
**Approved and assigned issue:** #4643

Resolves #4643

## Description

`TradeShipExecution` currently calls `WaterPathFinder.next()` and then
performs a second one-shot `findPath()` for the same destination when it
records a motion plan. `next()` has already calculated and cached the
traversal route, so the second call repeats the expensive water-path
calculation solely to recreate the route for the client.

This change lets `PathFinderStepper` return a copy of the active path
after `next()` advances it. Numeric paths remain `Uint32Array`s,
matching the compact representation already used by the stepper and
supported by motion-plan packing.

`WaterPathFinder.pathForTraversal()` retains the existing second
water-graph freshness check. If the graph refresh replaces the stepper
between `next()` and motion-plan recording, it falls back to the
original one-shot query. Otherwise it returns the cached remainder. The
method also normalizes the result to begin at the tile returned by
`next()`, so `TradeShipExecution` does not need to mutate the path.

`findPath()` remains a stateless one-shot query. The change is limited
to TradeShip and the minimal pathfinder support; it includes no
TransportShip changes.

## Performance

Current upstream `main` (`da2e0918`) was compared with the same commit
plus only this change. Each variant was run once per replay on macOS
arm64 with CPU profiling disabled. The replay intents were identical
between variants, and the current-client final hash and hash tick
matched for every pair.

| Game ID | Workload | Whole replay | TradeShip execution | Throughput |
| --- | --- | ---: | ---: | ---: |
| `DWmULj4H` | Antarctica, TradeShip-heavy | 68.960 s → 60.271 s
(**-12.60%**) | 18.981 s → 11.844 s (**-37.60%**) | 446 → 510 ticks/s |
| `efA7EZv9` | Australia | 14.660 s → 14.346 s (**-2.14%**) | 1.216 s →
0.920 s (**-24.34%**) | 441 → 450 ticks/s |
| `waxvMCue` | Svalmel | 16.666 s → 16.537 s (**-0

src/core/pathfinding/PathFinder.ts
@@ -127,7 +127,7 @@ export class PathFinding {
  * waterGraphVersion to stagger when each ship invalidates its cached path.
  */
 export class WaterPathFinder implements SteppingPathFinder<TileRef> {
-  private stepper: SteppingPathFinder<TileRef>;
+  private stepper: PathFinderStepper<TileRef>;
   private _waterGraphVersion: number;
   private _rebuilt = false;
 
@@ -195,9 +195,23 @@ export class WaterPathFinder implements SteppingPathFinder<TileRef> {
     return this.stepper.next(from, to, dist);
   }
 
+  /** Runs a one-shot query without changing the path consumed by next(). */
   findPath(from: TileRef | TileRef[], to: TileRef): TileRef[] | null {
     this.ensureFresh();
     return this.stepper.findPath(from, to);
+  }
+
+  /**
+   * Returns the route following a successful next() call, starting at `from`.
+   * If refreshing the water graph replaced the stepper, fall back to the same
+   * one-shot query used before traversal paths were reusable.
+   */
+  pathForTraversal(from: TileRef, to: TileRef): TileRef[] | Uint32Array {
+    this.ensureFresh();
+    const path =
+      this.stepper.pathAfterNext() ?? this.stepper.findPath(from, to);
+    if (path === null || path.length === 0) return [from];
+    return path[0] === from ? path : [from, ...path];
   }
 
   invalidate(): void {

johomax/ProxyWar

(cherry picked from commit 6dd9eb83233be89d0e222c38b12327b7b737ca92)

src/core/pathfinding/PathFinder.ts
@@ -10,7 +10,7 @@ import {
 } from "./PathFinder.Parabola";
 import { StationPathFinder } from "./PathFinder.Station";
 import { PathFinderBuilder } from "./PathFinderBuilder";
-import { StepperConfig } from "./PathFinderStepper";
+import { PathFinderStepper, StepperConfig } from "./PathFinderStepper";
 import { ComponentCheckTransformer } from "./transformers/ComponentCheckTransformer";
 import { MiniMapTransformer } from "./transformers/MiniMapTransformer";
 import { ShoreCoercingTransformer } from "./transformers/ShoreCoercingTransformer";
@@ -33,7 +33,7 @@ export class UniversalPathFinding {
  * Pathfinders that require Game - simulation layer only
  */
 export class PathFinding {
-  static Water(game: Game): SteppingPathFinder<TileRef> {
+  static Water(game: Game): PathFinderStepper<TileRef> {
     const pf = game.miniWaterHPA();
     const graph = game.miniWaterGraph();
 
@@ -52,7 +52,7 @@ export class PathFinding {
       .buildWithStepper(tileStepperConfig(game));
   }
 
-  static WaterSimple(game: Game): SteppingPathFinder<TileRef> {
+  static WaterSimple(game: Game): PathFinderStepper<TileRef> {
     const miniMap = game.miniMap();
     const pf = new AStarWater(miniMap);
 
@@ -94,7 +94,7 @@ export class PathFinding {
  * Wraps SteppingPathFinder and tracks waterGraphVersion internally.
  */
 export class WaterPathFinder implements SteppingPathFinder<TileRef> {
-  private inner: SteppingPathFinder<TileRef>;
+  private inner: PathFinderStepper<TileRef>;
   private _waterGraphVersion: number;
   private _rebuilt = false;
 
@@ -154,9 +154,23 @@ export class WaterPathFinder implements SteppingPathFinder<TileRef> {
     return this.inner.next(from, to, dist);
   }
 
+  /** Runs a one-shot query without changing the path consumed by next(). */
   findPath(from: TileRef | TileRef[], to: TileRef): TileRef[] | null {
     this.ensureFresh();
     return this.inner.findPath(from, to);
+  }
+
+  /**
+   * Returns the route following a successful next() call, starting at `from`.
+   * If refreshing the water graph replaced the stepper, fall back to the same
+   * one-shot query used before traversal paths were reusable.
+   */
+  pathForTraversal(from: TileRef, to: TileRef): TileRef[] | Uint32Array {
+    this.ensureFresh();
+    const path =
+      this.inner.pathAfterNext() ?? this.inner.findPath(from, to);
+    if (path === null || path.length === 0) return [from];
+    return path[0] === from ? path : [from, ...path];
   }
 
   invalidate(): void {